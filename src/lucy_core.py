79d26c604454fde3b50bc691a23fc2eb154b82b5
Sumit <sumitsingh.connect@gmail.com>
2026-03-05 23:09:49 +0530

    Lucy v0.01 - Emotion Detection Engine (TF-IDF + Logistic Regression )

---
+++ src/lucy_core.py
@@ -0,0 +1,22 @@
+import joblib
+
+
+model = joblib.load("models/lucy_model_v0_01.pkl")
+vectorizer = joblib.load("models/lucy_vectorizer_v0_01.pkl")
+
+
+def predict_emotion (text) :
+    text_vector = vectorizer.transform([text])
+    prediction = model.predict(text_vector)
+
+    return prediction[0]
+
+
+
+if __name__ == "__main__":
+    
+    user_input = input("You: ")
+    
+    emotion = predict_emotion(user_input)
+
+    print("Lucy:", emotion)
